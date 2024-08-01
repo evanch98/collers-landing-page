5eb2410e3b6aa353a8abca1cffa2fa3aa27c5302
Kyaw Thu <evanch98@gmail.com>
2024-08-01 22:51:47 +0800

    adding a new section

---
+++ components/hero/section/hero-section.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { Button } from '@/components/button/button';
+import { Shoe } from '@/components/hero/shoe/shoe';
+import { HeroText } from '@/components/hero/text/hero-text';
+import { ClientProvider } from '@/providers/client-provider';
+
+export const HeroSection = () => {
+  return (
+    <ClientProvider>
+      <section className="flex h-screen w-full flex-col items-center justify-center">
+        <div className="flex h-full w-full max-w-screen-2xl flex-col items-center justify-center gap-16 px-2 sm:px-4 lg:flex-row lg:px-8 2xl:gap-32 2xl:px-16">
+          <div className="flex max-w-[698px] flex-col items-center justify-center gap-y-8 lg:items-start lg:justify-start">
+            <HeroText />
+            <div className="flex gap-x-4">
+              <Button>Get Started</Button>
+              <Button variant="ghost">Explore</Button>
+            </div>
+          </div>
+          <Shoe />
+        </div>
+      </section>
+    </ClientProvider>
+  );
+};
