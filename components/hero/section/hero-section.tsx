71f2a38a49699df08e85d980bda0fd39d5fb52e1
Kyaw Thu <evanch98@gmail.com>
2024-08-02 19:49:24 +0800

    updating the styling to meet the design specification

--- components/hero/section/hero-section.tsx
+++ components/hero/section/hero-section.tsx
@@ -9,15 +9,17 @@ export const HeroSection = () => {
   return (
     <ClientProvider>
       <section className="flex h-screen w-full flex-col items-center justify-center">
-        <div className="flex h-full w-full max-w-screen-2xl flex-col items-center justify-center gap-16 px-2 sm:px-4 lg:flex-row lg:px-8 2xl:gap-32 2xl:px-16">
-          <div className="flex max-w-[698px] flex-col items-center justify-center gap-y-8 lg:items-start lg:justify-start">
+        <div className="flex h-full w-full max-w-screen-2xl flex-col items-center justify-center gap-8 px-2 sm:px-4 lg:flex-row lg:px-8 2xl:gap-32 2xl:px-16">
+          <div className="flex flex-col items-center justify-center gap-y-8 lg:items-start lg:justify-start">
             <HeroText />
             <div className="flex gap-x-4">
               <Button>Get Started</Button>
               <Button variant="ghost">Explore</Button>
             </div>
           </div>
-          <Shoe />
+          <div className="flex h-[261px] w-[304px] flex-shrink-0 items-center justify-center sm:h-[348px] sm:w-[400px] md:h-[423px] md:w-[486px]">
+            <Shoe />
+          </div>
         </div>
       </section>
     </ClientProvider>
