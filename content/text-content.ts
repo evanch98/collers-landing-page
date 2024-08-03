16bfb57466cf1773630f7148ea5d89dcfb3842c3
Kyaw Thu <evanch98@gmail.com>
2024-08-03 18:15:53 +0800

    adding new content

--- content/text-content.ts
+++ content/text-content.ts
@@ -55,3 +55,54 @@ export const benefits = {
   ],
   imgPath: '/assets/benefits-img.jpg',
 };
+
+export const gallery = {
+  jordan: [
+    'https://images.pexels.com/photos/15632278/pexels-photo-15632278/free-photo-of-close-up-of-white-and-purple-nike-air-jordan-1-sneakers.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/10853637/pexels-photo-10853637.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/13691727/pexels-photo-13691727.jpeg?auto=compress&cs=tinysrgb&w=600',
+    'https://images.pexels.com/photos/2529157/pexels-photo-2529157.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/14910512/pexels-photo-14910512.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/8987247/pexels-photo-8987247.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  ],
+  nike: [
+    'https://images.pexels.com/photos/13236696/pexels-photo-13236696.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1456738/pexels-photo-1456738.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/2119223/pexels-photo-2119223.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/2404959/pexels-photo-2404959.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1478442/pexels-photo-1478442.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1456706/pexels-photo-1456706.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  ],
+  adidas: [
+    'https://images.pexels.com/photos/20082772/pexels-photo-20082772/free-photo-of-brand-sneakers-on-display.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/11820505/pexels-photo-11820505.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/14174469/pexels-photo-14174469.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/14214618/pexels-photo-14214618.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/19737600/pexels-photo-19737600/free-photo-of-pair-of-brand-sneakers.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/8378567/pexels-photo-8378567.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  ],
+  puma: [
+    'https://images.pexels.com/photos/6698234/pexels-photo-6698234.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/6748334/pexels-photo-6748334.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/15036819/pexels-photo-15036819/free-photo-of-close-up-shot-of-a-puma-rubber-shoes.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/2759783/pexels-photo-2759783.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/6747236/pexels-photo-6747236.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/6776608/pexels-photo-6776608.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  ],
+  newBalance: [
+    'https://images.pexels.com/photos/1464625/pexels-photo-1464625.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/12506036/pexels-photo-12506036.jpeg?auto=compress&cs=tinysrgb&w=600',
+    'https://images.pexels.com/photos/12607446/pexels-photo-12607446.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/13691723/pexels-photo-13691723.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/12601013/pexels-photo-12601013.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/17745504/pexels-photo-17745504/free-photo-of-back-of-a-pair-of-new-balance-990-sneakers.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  ],
+  reebok: [
+    'https://images.pexels.com/photos/18946646/pexels-photo-18946646/free-photo-of-reebok-question-mid-nice-kicks-bubba-chuck-shoes.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/10050107/pexels-photo-10050107.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/20244251/pexels-photo-20244251/free-photo-of-white-sneakers-standing-on-a-blanket-on-a-bed.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/10050106/pexels-photo-10050106.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/10050109/pexels-photo-10050109.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/18328061/pexels-photo-18328061/free-photo-of-picture-of-the-legs-of-a-man-jumping.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  ],
+};
