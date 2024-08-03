a4985698bb845c27313e89b7fc6315dd407e02df
Kyaw Thu <evanch98@gmail.com>
2024-08-03 16:28:34 +0800

    adding the ProductsSection

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { FeatureSection } from '@/components/features/section/feature-section';
 import { HeroSection } from '@/components/hero/section/hero-section';
+import { ProductsSection } from '@/components/products/section/products-section';
 
 export default function Home() {
   return (
     <main>
       <HeroSection />
       <FeatureSection />
+      <ProductsSection />
     </main>
   );
 }
