b5a96c2d42de5485853c1b22fb42e852ae633643
Kyaw Thu <evanch98@gmail.com>
2024-08-04 17:49:17 +0800

    adding the New section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { BenefitsSection } from '@/components/benefits/section/benefits-section'
 import { FeatureSection } from '@/components/features/section/feature-section';
 import { GallerySection } from '@/components/gallery/section/gallery-section';
 import { HeroSection } from '@/components/hero/section/hero-section';
+import { NumberSection } from '@/components/number/section/number-section';
 import { ProductsSection } from '@/components/products/section/products-section';
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
       <ProductsSection />
       <BenefitsSection />
       <GallerySection />
+      <NumberSection />
     </main>
   );
 }
